6898dd2db6062dc5dc80c1bf9318dc060726b97a
sanpak <hillsfrontendweb@gmail.com>
2016-06-13 14:17:37 -0400

    playing the anonymous function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 $(function() {
 
-  function getCity() {
-    var city = $('#city-type').val();
-    console.log(city);
-  }
+  //create a function to get value from USER'S input such as what city they type.
+  
+  var city = $('#submit-btn').click(function() {
+    $('#city-type').val();
+  });
 
+ /* function getCity() {
+    $('#city-type').val();
+   }*/
+
+  //when users click submit button, it calls the function getCity.
   $('#submit-btn').click(getCity);
+
+  //if a user submits New York/New York City/NYC
+
+  if 
 
 
 });
