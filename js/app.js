87fefb12566f95f9402fa461f81441d89a2bcce1
sanpak <hillsfrontendweb@gmail.com>
2016-06-13 14:50:33 -0400

    I addClass() to body to switch to NYC background
    However, the page reload itself after I hit submit button. THE NYC
    background won't stay on.

--- js/app.js
+++ js/app.js
@@ -2,20 +2,24 @@ $(function() {
 
   //create a function to get value from USER'S input such as what city they type.
   
-  var city = $('#submit-btn').click(function() {
-    $('#city-type').val();
-  });
+  function processCity() {
+    var city = $('#city-type').val();
+    
 
- /* function getCity() {
-    $('#city-type').val();
-   }*/
+    //if a user submits New York/New York City/NYC
+    if (city === "New York City" || city === "New York" || city === "NYC") {
+      
+      $('body').addClass('nyc');
+    }
+
+   }
 
   //when users click submit button, it calls the function getCity.
-  $('#submit-btn').click(getCity);
+  $('#submit-btn').click(processCity);
 
-  //if a user submits New York/New York City/NYC
+  
 
-  if 
+  
 
 
 });
