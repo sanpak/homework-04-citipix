119f8a784fadbb6b430c2c49fb1de6e1dc0d2000
sanpak <hillsfrontendweb@gmail.com>
2016-06-19 13:49:49 -0400

    add if/else statements
    if/else statements to determine if user inputs match
    "NYC", "SF","LA","ATX","SYD"

--- js/app.js
+++ js/app.js
@@ -2,21 +2,36 @@ $(function() {
 
   //create a function to get value from USER'S input such as what city they type.
   
-  function processCity() {
-    var city = $('#city-type').val();
-    
+ $('#cities').submit(function(event) {
 
-    //if a user submits New York/New York City/NYC
-    if (city === "New York City" || city === "New York" || city === "NYC") {
-      
-      $('body').addClass('nyc');
+    //assign input cities to a variable
+    var city = $('#city-type').val();  
+
+    //prevent Default form behavior from happening
+    event.preventDefault();
+
+
+
+    console.log("city", city);
+
+    //if else statment to determine which background pcitures to show
+    if (city == "New York City" || "NYC" || "New York") {
+      $("body").addClass("nyc");
+    } else if (city === "San Francisco" || "SF" || "Bay Area") {
+      $("body").addClass("sf");
+    } else if (city === "Los Angeles" || "LA" || "LAX") {
+      $("body").addClass("la");
+    } else if (city === "Austin" || "ATX") {
+      $("body").addClass("austin");
+    } else if (city === "Sydney" || "SYD") {
+      $("body").addClass("sydney");
     }
 
-   }
+    //clear text field
+    $("#city-type").val("");
 
-  //when users click submit button, it calls the function getCity.
-  $('#submit-btn').click(processCity);
 
+  });
   
 
   
