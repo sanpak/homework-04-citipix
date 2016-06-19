1f2b5a823cc90b454a5db1db45f7a3f5d034f693
sanpak <hillsfrontendweb@gmail.com>
2016-06-19 15:17:55 -0400

    done.
    use toLowerCase(). so in the if/else statement. the user inputs are not
    case sensitive

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@ $(function() {
  $('#cities').submit(function(event) {
 
     //assign input cities to a variable
-    var city = $('#city-type').val();  
+    //change user input to all lower case
+    //so the input will not be case sensitive.
+    var city = ($('#city-type').val()).toLowerCase();  
 
     //prevent Default form behavior from happening
     event.preventDefault();
@@ -15,15 +17,29 @@ $(function() {
     console.log("city", city);
 
     //if else statment to determine which background pcitures to show
-    if (city == "New York City" || city === "NYC" || city === "New York") {
+    if (city == "new york city" || city === "nyc" || city === "new york") {
+      //if value of city = New York City or NYC or New York (not case sensitive)
+      //background pciture will be NYC jpg
       $("body").attr("class","nyc");
-    } else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
+
+    } else if (city === "san francisco" || city === "sf" || city === "bay area") {
+      //if value of city = San Francisco or SF or Bay Area (not case sensitive)
+      //background picture will be SF jpg
       $("body").attr("class","sf");
-    } else if (city === "Los Angeles" || city === "LA" || city === "LAX") {
+
+    } else if (city === "los angeles" || city === "la" || city === "lax") {
+      //if value of city = Los Angeles or LA or LAX (not case sensitive)
+      //background picture will be LA jpg
       $("body").attr("class","la");
-    } else if (city === "Austin" || city === "ATX") {
+
+    } else if (city === "austin" || city === "atx") {
+      //if value of city = Austin or ATX (not case sensitive)
+      //background picture will be austin jpg
       $("body").attr("class","austin");
-    } else if (city === "Sydney" || city === "SYD") {
+
+    } else if (city === "sydney" || city === "syd") {
+      //if value of city = Sydney or SYD (not case sensitive)
+      //background picture will sydney jpg
       $("body").attr("class","sydney");
     }
 
