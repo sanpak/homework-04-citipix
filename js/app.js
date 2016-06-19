9126128528ba50a522607cb947c796444fc056bb
sanpak <hillsfrontendweb@gmail.com>
2016-06-19 14:49:06 -0400

    use attr() instead of addClass()
    addClass() keeping adding classes. That's not what I want. attr() swaps
    the class.

--- js/app.js
+++ js/app.js
@@ -15,16 +15,16 @@ $(function() {
     console.log("city", city);
 
     //if else statment to determine which background pcitures to show
-    if (city == "New York City" || "NYC" || "New York") {
-      $("body").addClass("nyc");
-    } else if (city === "San Francisco" || "SF" || "Bay Area") {
-      $("body").addClass("sf");
-    } else if (city === "Los Angeles" || "LA" || "LAX") {
-      $("body").addClass("la");
-    } else if (city === "Austin" || "ATX") {
-      $("body").addClass("austin");
-    } else if (city === "Sydney" || "SYD") {
-      $("body").addClass("sydney");
+    if (city == "New York City" || city === "NYC" || city === "New York") {
+      $("body").attr("class","nyc");
+    } else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
+      $("body").attr("class","sf");
+    } else if (city === "Los Angeles" || city === "LA" || city === "LAX") {
+      $("body").attr("class","la");
+    } else if (city === "Austin" || city === "ATX") {
+      $("body").attr("class","austin");
+    } else if (city === "Sydney" || city === "SYD") {
+      $("body").attr("class","sydney");
     }
 
     //clear text field
